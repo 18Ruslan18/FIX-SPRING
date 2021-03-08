3aba6c1f8f22a7b8a35450064e3211b5e1b9aad3
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-09 01:09:06 +0300

    someField.set(someObject, 777);

--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -3,7 +3,7 @@ package demo;
 import java.lang.reflect.Field;
 
 public class Main {
-    public static void main(String[] argv) throws NoSuchFieldException {
+    public static void main(String[] argv) throws NoSuchFieldException, IllegalAccessException {
         SomeClass someObject = new SomeClass();
         Class<SomeClass> someClassAsClass = (Class<SomeClass>)someObject.getClass();
         Field someField = someClassAsClass.getField("someField");
@@ -12,5 +12,8 @@ public class Main {
         for (Field field : fields) {
             System.out.println(field.getType() + " " + field.getName());
         }
+        System.out.println(someObject.someField);
+        someField.set(someObject, 777);
+        System.out.println(someObject.someField);
     }
 }

3ba6e5e0aa2d4f4696e6c930b3f9c8e098d2198e
18Ruslan18 <79464524+18Ruslan18@users.noreply.github.com>
2021-03-09 01:31:32 +0300

    Class.forName

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,18 @@
+package classes;
+
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String className = scanner.next();
+        Class aClass = Class.forName(className);
+
+        Field fields[] = aClass.getFields();
+
+        for (Field field : fields) {
+            System.out.println(field.getType() + " " + field.getName());
+        }
+    }
+    }
